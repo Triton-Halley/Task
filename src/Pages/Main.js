ddb4724241ad8fa7f0dde2b35a98f6566da9daa9
Ali Geshani <Triton.halley@outlook.com>
2022-08-18 00:28:00 +0430

    add Components to Main Page

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,9 +1,14 @@
 import NavigationMenu from "../Components/NavigationMenu";
-
+import Pagination from "../Components/Pagination";
+import Search from "../Components/Search";
+import GridList from "../Components/Grid/GridList";
 function Main() {
   return (
     <>
       <NavigationMenu />
+      <Search />
+      <GridList />
+      <Pagination />
     </>
   );
 }
