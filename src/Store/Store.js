7c565749f847a9af514e20df8aca8633008abada
Ali Geshani <Triton.halley@outlook.com>
2022-08-18 22:07:58 +0430

    add slice reducers to store

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import DataSlice from "./DataSlice";
 import AuthenticationSlice from "./AuthenticationSlice";
+import DisplayGridItemsSlice from "./DisplayGirdItemsSlice";
 const Store = configureStore({
-  reducer: { data: DataSlice.reducer, auth: AuthenticationSlice.reducer },
+  reducer: {
+    data: DataSlice.reducer,
+    auth: AuthenticationSlice.reducer,
+    gridView: DisplayGridItemsSlice.reducer,
+  },
 });
 
 export default Store;
