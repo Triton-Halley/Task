03b1326ca2c9499ca9818eb8933fbd358aca4749
Ali Geshani <Triton.halley@outlook.com>
2022-08-19 02:03:13 +0430

    add event action to open modal with posts buttons

--- src/Components/Grid/GridItem.js
+++ src/Components/Grid/GridItem.js
@@ -1,4 +1,11 @@
+import { useDispatch } from "react-redux";
+import { ModalAction } from "../../Store/ModalSlice";
 function GridItem({ title, id, desc }) {
+  const dispatch = useDispatch();
+  const openEditModal = () => {
+    dispatch(ModalAction.openModal());
+    dispatch(ModalAction.setData({ title, desc, id }));
+  };
   return (
     <>
       <div className="flex md:justify-center">
@@ -8,6 +15,7 @@ function GridItem({ title, id, desc }) {
           </h5>
           <p className="text-gray-700 text-base mb-4">{desc}</p>
           <button
+            onClick={openEditModal}
             type="button"
             className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
           >
