f421e3472092bdee6db93e09ea1b6bfe99ec200e
Ali Geshani <Triton.halley@outlook.com>
2022-08-19 00:07:27 +0430

    add filtering state

--- src/Store/DataSlice.js
+++ src/Store/DataSlice.js
@@ -2,12 +2,22 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const DataSlice = createSlice({
   name: "data",
-  initialState: { data: [] },
+  initialState: {
+    data: [],
+    isFilter: false,
+    FilteredArray: [],
+    searchResult: null,
+  },
   reducers: {
     editItem(state, action) {},
     fetchItem(state, action) {
       state.data = action.payload;
       console.log(state.data);
+    },
+    filtering(state, action) {
+      state.isFilter = true;
+      state.FilteredArray = action.payload.filter;
+      state.searchResult = action.payload.filter.length;
     },
   },
 });
