3550471f8afc29f488bbb8df92cec54d0eb438da
Ali Geshani <Triton.halley@outlook.com>
2022-08-18 22:08:52 +0430

    add Grid Item component

--- src/Components/Grid/GridItem.js
+++ src/Components/Grid/GridItem.js
@@ -1,2 +1,21 @@
-function GridItem() {}
+function GridItem({ title, id, desc }) {
+  return (
+    <>
+      <div className="flex">
+        <div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
+          <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">
+            {title}
+          </h5>
+          <p className="text-gray-700 text-base mb-4">{desc}</p>
+          <button
+            type="button"
+            className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+          >
+            edit
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
 export default GridItem;
