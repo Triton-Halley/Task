6847112ef16e2f9fe1c398da71828bd65ea8ff03
Ali Geshani <Triton.halley@outlook.com>
2022-08-19 00:08:38 +0430

    add search logic

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,4 +1,29 @@
+import { useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { DataSliceAction } from "../Store/DataSlice";
+import { DisplayGridItemsAction } from "../Store/DisplayGirdItemsSlice";
+
 function Search() {
+  const dispatch = useDispatch();
+  const AllPosts = useSelector((state) => state.data.data);
+  const searchInputRef = useRef();
+  const searchHandler = () => {
+    const searchEntered = searchInputRef.current.value;
+
+    if (searchEntered.trim().length < 0) {
+      return;
+    }
+
+    const filtered = AllPosts.filter(
+      (value) =>
+        value.title.includes(searchEntered) ||
+        value.body.includes(searchEntered)
+    );
+    console.log(filtered);
+    dispatch(DataSliceAction.filtering({ filter: filtered }));
+    const cutData = filtered.slice(0, 9);
+    dispatch(DisplayGridItemsAction.updateGridView({ data: cutData }));
+  };
   return (
     <>
       <div className="flex justify-center mt-5">
@@ -8,8 +33,10 @@ function Search() {
               type="search"
               className="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
               placeholder="Search"
+              ref={searchInputRef}
             />
             <button
+              onClick={searchHandler}
               className="btn inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex items-center"
               type="button"
               id="button-addon2"
