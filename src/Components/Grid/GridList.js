898d4d4166310d7deec1aba1ba9547e3d1857442
Ali Geshani <Triton.halley@outlook.com>
2022-08-18 22:08:31 +0430

    add Grid List Container

--- src/Components/Grid/GridList.js
+++ src/Components/Grid/GridList.js
@@ -1,3 +1,25 @@
-function GridList() {}
+import React from "react";
+import GridItem from "./GridItem";
+import { useSelector } from "react-redux";
+function GridList() {
+  const fetchedData = useSelector((state) => state.gridView.gridListItems);
+  console.log("fetched data", fetchedData);
+  return (
+    <>
+      <div className="grid grid-cols-3 justify-center gap-4 m-auto lg:w-2/3 ">
+        {fetchedData.map((item) => {
+          return (
+            <GridItem
+              key={item.id}
+              id={item.id}
+              title={item.title}
+              desc={item.body}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+}
 
 export default GridList;
