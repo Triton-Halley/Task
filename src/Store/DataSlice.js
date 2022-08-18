76c619dec7e51fa1b2d7dd65c10406add9bcd5d2
Ali Geshani <Triton.halley@outlook.com>
2022-08-18 22:06:56 +0430

    add data slice to hold all data

--- src/Store/DataSlice.js
+++ src/Store/DataSlice.js
@@ -2,9 +2,13 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const DataSlice = createSlice({
   name: "data",
-  initialState: [],
+  initialState: { data: [] },
   reducers: {
     editItem(state, action) {},
+    fetchItem(state, action) {
+      state.data = action.payload;
+      console.log(state.data);
+    },
   },
 });
 
