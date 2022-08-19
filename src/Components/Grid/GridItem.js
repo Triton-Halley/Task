896530b3f21e16257a3c732837d9274c86bed233
Ali Geshani <Triton.halley@outlook.com>
2022-08-19 15:29:41 +0430

    change design tailwind CSS

--- src/Components/Grid/GridItem.js
+++ src/Components/Grid/GridItem.js
@@ -9,15 +9,17 @@ function GridItem({ title, id, desc }) {
   return (
     <>
       <div className="flex md:justify-center">
-        <div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
+        <div className="flex flex-col p-6  rounded-lg shadow-lg bg-white max-w-sm h-[278px]">
           <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">
             {title}
           </h5>
-          <p className="text-gray-700 text-base mb-4">{desc}</p>
+          <p className="text-gray-700 w-[336px] h-[200px] text-base mb-4">
+            {desc}
+          </p>
           <button
             onClick={openEditModal}
             type="button"
-            className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            className=" inline-block w-fit px-6 justify-self-end py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
           >
             edit
           </button>
--- src/Components/NavigationMenu.js
+++ src/Components/NavigationMenu.js
@@ -34,7 +34,7 @@ function NavigationMenu() {
                 <NavLink
                   className={(navData) =>
                     navData.isActive
-                      ? " text-white hover:text-gray-700 focus:text-gray-700 p-2 rounded active:bg-blue-500"
+                      ? "text-white hover:text-gray-700 focus:text-gray-700 p-2 rounded bg-blue-500"
                       : " text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0"
                   }
                   to={"/"}
